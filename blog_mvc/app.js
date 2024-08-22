08a6dfd9ec8100dc31f12b334019ee215dee3cea
Mohamed <mmuseaden@gmail.com>
2024-08-22 19:42:33 +0100

    blog_mvc set up and docker-compose file set up

--- blog_mvc/app.js
+++ blog_mvc/app.js
@@ -12,9 +12,23 @@ app.use(cors());
 app.use(express.json());
 app.use(logger);
 
+console.log("app js inside! NODE_ENV is?!", process.env.NODE_ENV);
+console.log("app js inside! BIG SECRET? is?!", process.env.JWT_SECRET);
+console.log("app js inside! from docker!! is?!", process.env.DOCKER_SECRET);
+console.log("app js inside! CHECK_OUT_DOCKER_ENV FROM DOCKER!", process.env.CHECK_OUT_DOCKER_ENV);
+console.log("app js inside! CHECK_OUT_DOCKER_ENV FROM DOCKER! value = ", process.env.CHECK_OUT_DOCKER_ENV);
+console.log("app js inside! INNER_SECRET I from inside the app! value = ", process.env.INNER_SECRET);
+
+
 app.use("/users", userRouter);
 app.use("/blogs", blogRouter);
 app.use("/comments", commentRouter);
 app.use("/replies", replyRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Sever is healthy and it is running!"
+  });
+});
 
 module.exports = app;
--- blog_mvc/db/connect.js
+++ blog_mvc/db/connect.js
@@ -1,9 +1,18 @@
 const { Pool } = require("pg");
 
-const db = new Pool({
-    connectionString: process.env.DB_URL
-});
+console.log("from docker", process.env.DOCKER_SECRET);
+console.log("NODE_ENV CHECK", process.env.NODE_ENV);
 
+const option = process.env.NODE_ENV === "test" ? {
+    connectionString: process.env.DB_TEST_URL } : {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT || 5432
+};
+
+const db = new Pool(option);
 console.log("DB connection established.");
 
 module.exports = db;
